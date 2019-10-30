9a4775fe3b05e8f1def641ccaf8949d1b0613e8c
nsap19 <zzz0484@naver.com>
2019-10-30 20:27:54 +0900

    first commit

---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, UserSerializerWithToken
+
+
+@api_view(['GET'])
+def current_user(request):
+       
+    serializer = UserSerializer(request.user) #현재 인증된 유저의 데이터만 가져와라
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+   
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
